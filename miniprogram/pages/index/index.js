d92e1eacad3c4a71cbc6a3813341620c9810de4a
kl_wu <kl_wu@Ctrip.com>
2019-06-16 20:22:07 +0800

    FareLock

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,77 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    array:[]
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+
+    const db = wx.cloud.database({
+      env: 'farelock-hswna'
+    })
+    db.collection('FareLock').where({
+      type: '1'
+    }).get({
+      success: res => {
+        console.log(res);
+       this.setData({
+         array:res.data
+       });
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  cutTicket:function(e){
+    var id=e.target.id;
+    wx.navigateTo({
+      url: '/pages/cut/cut?id='+id,
+    })
+  }
+
+})

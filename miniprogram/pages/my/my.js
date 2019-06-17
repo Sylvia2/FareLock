0ed4431e72731c1a1cf743ef1513e74981a71f18
LLH <1367115771@qq.com>
2019-06-17 22:47:15 +0800

    change UI

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentTab: 0
   },
 
   /**
@@ -62,5 +62,25 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  //滑动切换
+  swipeTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   }
 })
